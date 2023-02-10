67f353c9ec51b5db8abeed56653fac7380b6561c
kegma1 <61615726+kegma1@users.noreply.github.com>
2023-02-10 19:51:09 +0100

    worked om lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,24 +4,64 @@ pub enum Lexem {
     Operator(String),
     Literal(String),
     Keyword(String),
-    Separator(String),
-    Indent
+    _Separator(String),
+    Indent,
 }
+
+const KEYWORDS: [&str; 3] = ["funk", "konst", "var"];
+
+const OPERATOR: [&str; 5] = ["+", "-", "*", "/", "%"];
+
+const _SEPERATORS: [char; 3] = [':', '(', ')'];
 
 pub fn lex(src: &String) -> Result<Vec<Lexem>, &'static str> {
     let mut current_word = "".to_string();
     let mut lexed_src: Vec<Lexem> = vec![];
-    for c in src.chars() {
-        current_word.push(c);
-        
-        match current_word.as_str() {
-            "    " => {
-                lexed_src.push(Lexem::Indent);
-                current_word = "".to_string()
+    let mut i = 0;
+    while i < src.len() {
+        let c = src.chars().nth(i).unwrap();
+        match c {
+            '"' => {
+                let (str, offset) = scan_string(&src[i + 1..src.len()]).unwrap();
+                lexed_src.push(Lexem::Literal(str));
+                i += offset
             }
-            _ => ()
+            _ if c.is_whitespace() => {
+                println!("'{}'", current_word);
+                if current_word == "    " || current_word == "\t" {
+                    lexed_src.push(Lexem::Indent);
+                } else if KEYWORDS.contains(&current_word.as_ref()) {
+                    lexed_src.push(Lexem::Keyword(current_word));
+                } else if OPERATOR.contains(&current_word.as_ref()) {
+                    lexed_src.push(Lexem::Operator(current_word));
+                } else {
+                    if current_word != "" {
+                        lexed_src.push(Lexem::Identifier(current_word));
+                    }
+                }
+                current_word = "".to_string();
+            }
+            _ => current_word.push(c),
         }
+
+        i += 1
     }
 
     Ok(lexed_src)
+}
+
+fn scan_string(str: &str) -> Option<(String, usize)> {
+    let mut scanned_str = "".to_string();
+    let mut i_offset = 0;
+
+    for c in str.chars() {
+        if c != '"' {
+            scanned_str.push(c);
+            i_offset += 1;
+        } else {
+            break;
+        }
+    }
+
+    Some((scanned_str.clone(), i_offset + 1))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 mod lexer;
 
+use lexer::lex;
 use std::env;
 use std::fs;
-use lexer::lex;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     let path: Option<&String> = args.get(1);
 
     if let Some(p) = path {
-        let source_code = fs::read_to_string(p)
-            .expect(&format!("Kunne ikke lese fil '{}'", p));
-        println!("{:?}", lex(&source_code))
+        let source_code = fs::read_to_string(p).expect(&format!("Kunne ikke lese fil '{}'", p));
+        println!("{:?}", lex(&String::from(source_code + " ")).unwrap())
     } else {
         println!("no argument given")
     }
 }
-
