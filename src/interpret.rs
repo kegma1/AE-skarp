60b1cb80a75d26fc6a0c92e7119505f0f3ff2fee
kegma1 <61615726+kegma1@users.noreply.github.com>
2023-03-05 15:00:08 +0100

    started working on interperating

---
+++ src/interpret.rs
@@ -0,0 +1,6 @@
+use crate::common::Type;
+use crate::parser::Op;
+
+pub fn interpret(ast: Vec<Op>) -> Result<u8, &'static str> {
+    Ok(0)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod common;
+mod interpret;
 mod lexer;
 mod parser;
 
+use interpret::interpret;
 use lexer::lex;
 use parser::parse;
 use std::env;
@@ -14,9 +16,9 @@ fn main() {
     if let Some(p) = path {
         let source_code = fs::read_to_string(p).expect(&format!("Kunne ikke lese fil '{}'", p));
         let lexed = lex(&String::from(source_code + " ")).unwrap();
-
+        let parsed = parse(lexed).unwrap();
         // println!("{:?}", lexed)
-        println!("{:?}", parse(lexed).unwrap())
+        println!("{:?}", interpret(parsed))
     } else {
         println!("no argument given")
     }
